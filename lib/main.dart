e8cc5601521b6eeebb5b1df27c339aa68f660a98
Rayen Korbi <Rayen.Korbi@esprit.tn>
2026-01-30 12:52:26 +0100

    splash screen added + game scoring fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/title_screen.dart';
+import 'screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const TitleScreen(),
+      home: const SplashScreen(),
     );
   }
 }
